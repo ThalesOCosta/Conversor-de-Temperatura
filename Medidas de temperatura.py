139c3c26fbefaa20806c053fd6638c29e53d9bb9
ThalesOCosta <105451422+ThalesOCosta@users.noreply.github.com>
2022-05-12 16:09:05 -0300

    Add files via upload

---
+++ Medidas de temperatura.py
@@ -0,0 +1,19 @@
+# C - F
+c = float(input('Celsius: '))
+f = c * (9/5) + 32
+print(f'Fahrenheit: {f}')
+
+# F - C
+f = float(input('Fahrenheit: '))
+c = 5 * (f - 32) / 9
+print(f'Celsius: {c}')
+
+# C - K
+c = float(input('Celsius: '))
+k = c - 273.15
+print(f'Kelvin: {k}')
+
+# K - C
+k = float(input('Kelvin: '))
+c = k + 273.15
+print(f'Celsius: {c}')
